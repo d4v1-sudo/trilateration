7eb0c165d754d90d0166ef8d690d53e7c4287ba9
Davi S. Dal Pont <139714828+d4v1-sudo@users.noreply.github.com>
2023-10-17 08:43:42 -0300

    trilateration-oblate-spheroid.py

--- trilateration.py
+++ trilateration.py
@@ -1,68 +1,72 @@
 import math
 import numpy
 
-earthR = 6371
-LatA = float(input("Latitude of point A: "))
-LonA = float(input("Longitude of point A: "))
-AltA = float(input("Altitude of point A (in kilometers): "))
-DistA = float(input("Your distance to point A (in kilometers): "))
-print()
-LatB = float(input("Latitude of point B: "))
-LonB = float(input("Longitude of point B: "))
-AltB = float(input("Altitude of point B (in kilometers): "))
-DistB = float(input("Your distance to point B (in kilometers): "))
-print()
-LatC = float(input("Latitude of point C: "))
-LonC = float(input("Longitude of point C: "))
-AltC = float(input("Altitude of point C (in kilometers): "))
-DistC = float(input("Your distance to point C (in kilometers): "))
-print()
+# Polar and equatorial radii of the Earth in meters
+polar_radius = 6356752
+equatorial_radius = 6378137
 
-# Using an authalic sphere
-# If using an ellipsoid, this step is slightly different
-# Convert geodetic Latitude/Longitude to ECEF (Earth-Centered, Earth-Fixed) coordinates (xyz)
-#   1. Convert Latitude/Longitude to radians
-#   2. Convert Latitude/Longitude (in radians) to ECEF
-xA = (earthR + AltA) * (math.cos(math.radians(LatA)) * math.cos(math.radians(LonA)))
-yA = (earthR + AltA) * (math.cos(math.radians(LatA)) * math.sin(math.radians(LonA)))
-zA = (earthR + AltA) * (math.sin(math.radians(LatA)))
+LatA = math.radians(float(input("Latitude of point A: ")))
+LonA = math.radians(float(input("Longitude of point A: ")))
+AltA = float(input("Altitude of point A (in meters): "))
+DistA = float(input("Your distance to point A (in meters): ")
 
-xB = (earthR + AltB) * (math.cos(math.radians(LatB)) * math.cos(math.radians(LonB)))
-yB = (earthR + AltB) * (math.cos(math.radians(LatB)) * math.sin(math.radians(LonB)))
-zB = (earthR + AltB) * (math.sin(math.radians(LatB)))
+LatB = math.radians(float(input("Latitude of point B: ")))
+LonB = math.radians(float(input("Longitude of point B: ")))
+AltB = float(input("Altitude of point B (in meters): "))
+DistB = float(input("Your distance to point B (in meters): ")
 
-xC = (earthR + AltC) * (math.cos(math.radians(LatC)) * math.cos(math.radians(LonC)))
-yC = (earthR + AltC) * (math.cos(math.radians(LatC)) * math.sin(math.radians(LonC)))
-zC = (earthR + AltC) * (math.sin(math.radians(LatC)))
+LatC = math.radians(float(input("Latitude of point C: ")))
+LonC = math.radians(float(input("Longitude of point C: ")))
+AltC = float(input("Altitude of point C (in meters): "))
+DistC = float(input("Your distance to point C (in meters): ")
 
-P1 = numpy.array([xA, yA, zA])
-P2 = numpy.array([xB, yB, zB])
-P3 = numpy.array([xC, yC, zC])
+# Convert geodetic coordinates (latitude, longitude, and altitude) to ECEF coordinates
+def geodetic_to_ecef(lat, lon, alt):
+    N = equatorial_radius / math.sqrt(1 - (polar_radius / equatorial_radius) ** 2 * math.sin(lat) ** 2)
+    x = (N + alt) * math.cos(lat) * math.cos(lon)
+    y = (N + alt) * math.cos(lat) * math.sin(lon)
+    z = (N * (1 - (polar_radius / equatorial_radius) ** 2) + alt) * math.sin(lat)
+    return x, y, z
 
-# From Wikipedia
-# Transform to get circle 1 at the origin
-# Transform to get circle 2 on the x-axis
-ex = (P2 - P1) / (numpy.linalg.norm(P2 - P1))
-i = numpy.dot(ex, P3 - P1)
-ey = (P3 - P1 - i * ex) / (numpy.linalg.norm(P3 - P1 - i * ex))
-ez = numpy.cross(ex, ey)
-d = numpy.linalg.norm(P2 - P1)
-j = numpy.dot(ey, P3 - P1)
+xA, yA, zA = geodetic_to_ecef(LatA, LonA, AltA)
+xB, yB, zB = geodetic_to_ecef(LatB, LonB, AltB)
+xC, yC, zC = geodetic_to_ecef(LatC, LonC, AltC)
 
-# From Wikipedia
-# Plug and chug using the above values
-x = (pow(DistA, 2) - pow(DistB, 2) + pow(d, 2)) / (2 * d)
-y = ((pow(DistA, 2) - pow(DistC, 2) + pow(i, 2) + pow(j, 2)) / (2 * j)) - ((i / j) * x)
+AB = numpy.array([xB - xA, yB - yA, zB - zA])
+AC = numpy.array([xC - xA, yC - yA, zC - zA])
 
-# Only one case shown here
-z = numpy.sqrt(pow(DistA, 2) - pow(x, 2) - pow(y, 2))  # Height
+# Calculate the cross product of vectors AB and AC to obtain the normal of the plane formed by the points.
+N = numpy.cross(AB, AC)
+N /= numpy.linalg.norm(N)  # Normalize the normal.
 
-# triPt is an array with ECEF x, y, z of the trilateration point
-triPt = P1 + x * ex + y * ey + z * ez
+# Use the dot product to find the distances in the direction of each point.
+dA = numpy.dot(N, numpy.array([xA, yA, zA]))
+dB = numpy.dot(N, numpy.array([xB, yB, zB]))
+dC = numpy.dot(N, numpy.array([xC, yC, zC]))
 
-# Convert back to Latitude/Longitude from ECEF
-# Convert to degrees
-lat = math.degrees(math.asin(triPt[2] / (earthR + AltA)))
-lon = math.degrees(math.atan2(triPt[1], triPt[0]))
+# Use the distances to calculate the coordinates of the trilateration point.
+x_trilateration = (dA * xA + dB * xB + dC * xC) / (dA + dB + dC)
+y_trilateration = (dA * yA + dB * yB + dC * yC) / (dA + dB + dC)
+z_trilateration = (dA * zA + dB * zB + dC * zC) / (dA + dB + dC)
 
-print(lat, lon)
+# Now you have the ECEF coordinates of the trilateration point on the oblate spheroid.
+# You can convert these ECEF coordinates into geodetic coordinates (latitude, longitude, and altitude)
+# to obtain the location in terms of Earth's latitude and longitude.
+
+# Function to convert ECEF coordinates to geodetic coordinates
+def ecef_to_geodetic(x, y, z):
+    p = math.sqrt(x ** 2 + y ** 2)
+    theta = math.atan2(z * equatorial_radius, p * polar_radius)
+    latitude = math.atan2((z + polar_radius * polar_radius * polar_radius * math.sin(theta) * math.sin(theta) * math.sin(theta)),
+                         (p - equatorial_radius * equatorial_radius * equatorial_radius * math.cos(theta) * math.cos(theta) * math.cos(theta)))
+    longitude = math.atan2(y, x)
+    N = equatorial_radius / math.sqrt(1 - polar_radius * polar_radius * math.sin(latitude) * math.sin(latitude))
+    altitude = p / math.cos(latitude) - N
+    return math.degrees(latitude), math.degrees(longitude), altitude
+
+# Convert the ECEF coordinates of the trilateration point to geodetic coordinates
+lat_trilateration, lon_trilateration, alt_trilateration = ecef_to_geodetic(x_trilateration, y_trilateration, z_trilateration)
+
+print(f"Latitude of trilateration: {lat_trilateration} degrees")
+print(f"Longitude of trilateration: {lon_trilateration} degrees")
+print(f"Altitude of trilateration: {alt_trilateration} meters")
